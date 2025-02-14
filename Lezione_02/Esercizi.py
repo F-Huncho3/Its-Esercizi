133a9a7f996a953900138913ed51532375b1696e
F-Huncho3 <f.magnoawad@its-ictacademy.com>
2025-02-14 13:51:10 +0100

    First Commit

---
+++ Lezione_02/Esercizi.py
@@ -0,0 +1,20 @@
+#1-1
+x:float = 1.5
+y:float = 1.0/x 
+print(x,y,y*x)
+print((x*y)-x)
+
+#1-2
+x:float = 4.0
+y:float = (x%2.0)
+print (x)
+print (y)
+
+x:float = -4.0
+y:float = (x%2.0)
+print (x)
+print (y)
+'''Siccome x è "divisible" per due in entrambi i casi 
+il risultato dell'operazione è lo stesso
+'''
+#1-3
