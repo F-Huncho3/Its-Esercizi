304790a37d1f639094a54031788791c572154607
F-Huncho3 <f.magnoawad@its-ictacademy.com>
2025-02-18 18:01:40 +0100

    First Commit

---
+++ Lezione_03/Esercizo Posizioni Gara.py
@@ -0,0 +1,13 @@
+n:int = (int(input("Write your finishing position ")))
+
+if n == 1:
+    print("1st")
+    
+elif n == 2:
+    print("2nd")
+
+elif n >= 3:
+    print(f"{n}th")
+
+else:
+    print("Number not valid")
