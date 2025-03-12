4393f41c1df0add9f5d2f3c6cc3350ab4280df37
F-Huncho3 <f.magnoawad@its-ictacademy.com>
2025-03-12 09:23:47 +0100

    Deleted Folders

--- COMPITO/ESERCIZIO-2.py
+++
@@ -1,9 +0,0 @@
-x:str = str(input("Inserisci una frase "))
-
-spazi = 0 
-
-for char in x:
-    if char == " ":
-        spazi += 1 
-
-print (f"I tuoi spazi sono {spazi}") 
--- COMPITO/ESERCIZIO-3.py
+++
@@ -1,8 +0,0 @@
-parola_corretta:str = (input("Inserisci una parola "))
-y:str = " "
-
-for char in parola_corretta:
-    y = char + y
-
-print (f"La parola invertita è {y}")
-
